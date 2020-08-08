2425151daef2a055c4f5f421544de81296f4e6ba
Fernando Pires <alariofernandopires@gmail.com>
2020-08-08 03:22:49 -0300

    Adição de duas rotas no backend"

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,18 @@
 import express from 'express';
+import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    const users = [
-        {"name":"Eu", "gender":"M"},
-        {"name":"Voce","gender":"F"},
-        {"name":"Zoboomafoo","gender":"F"}
-    ]
-    return response.json(users)
-});
+// Importar para termos objeto json
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-app.post('/users', (request, response) => {
-    const users = [
-        {"name":"Eu", "gender":"M"},
-        {"name":"Voce","gender":"F"},
-        {"name":"Zoboomafoo","gender":"F"}
-    ]
-    return response.json(users)
-});
+// GET: Buscar ou listar infos
+// POST: Criar nova info
+// PUT: Atualizar info existente
+// DELETE: Deletar info
 
 // localhost:3333
 app.listen(3333);
